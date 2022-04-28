031675bc12d1385e1040baef26969d143ae6f5d0
YeeSkywalker <itsyeelin@gmail.com>
2022-04-27 23:24:24 -0600

    Update decoder.py

--- 414NET/decoder.py
+++ 414NET/decoder.py
@@ -9,6 +9,7 @@ import matplotlib
 matplotlib.use("agg")
 import matplotlib.pyplot as plt
 
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--model', type=str, default='', help='model')
@@ -20,19 +21,23 @@ parser.add_argument('--object', type=str, default='', help='object type')
 args = parser.parse_args()
 print(args)
 
+# Load data into generator
 data_set = DatasetGenerator(path=args.dataset, object_type= args.object, train_flag=False)
 
 content_idx = args.content
 style_idx = args.style
 
+# Load content point cloud
 content_point, content_seg = data_set[content_idx]
 print(content_point.size(), content_seg.size())
 point_np_content = content_point.numpy()
 
+# Load style point cloud
 style_point, style_seg = data_set[style_idx]
 print(style_point.size(), style_seg.size())
 point_np_style = style_point.numpy()
 
+# Load saved model to segment objects
 state_dict = torch.load(args.model)
 classifier = SegmentNet(out=state_dict['conv4.weight'].size()[0])
 classifier.load_state_dict(state_dict)
@@ -41,20 +46,25 @@ classifier.eval()
 content_point = content_point.transpose(1, 0).contiguous()
 style_point = style_point.transpose(1, 0).contiguous()
 
+# Segment content point cloud
 content_point = Variable(content_point.view(1, content_point.size()[0], content_point.size()[1]))
 pred_content, _ = classifier(content_point)
 pred_choice_content = pred_content.data.max(2)[1]
 
+# Segment style point cloud
 style_point = Variable(style_point.view(1, style_point.size()[0], style_point.size()[1]))
 pred_style, _ = classifier(style_point)
 pred_choice_style = pred_style.data.max(2)[1]
 
+# Load color map
 cmap = plt.cm.get_cmap("hsv", 10)
 cmap = np.array([cmap(i) for i in range(10)])[:, :3]
 
+# Map color into point clouds
 pred_color_content = cmap[pred_choice_content.numpy()[0], :]
 pred_color_style = cmap[pred_choice_style.numpy()[0], :]
 
+# Get segmented parts of the content point cloud
 content_dict = {}
 counter_content  = 0
 for index in range(len(point_np_content)):
@@ -64,6 +74,7 @@ for index in range(len(point_np_content)):
     else:
         content_dict[tuple(pred_color_content[index])].append(point_np_content[index].tolist())
 
+# Get segmented parts of the style point cloud
 style_dict = {}
 counter_style  = 0
 for index in range(len(point_np_style)):
@@ -73,6 +84,7 @@ for index in range(len(point_np_style)):
     else:
         style_dict[tuple(pred_color_style[index])].append(point_np_style[index].tolist())
 
+# Generate the output point cloud
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.view_init(elev=None, azim=None)
@@ -107,6 +119,7 @@ plt.savefig('output.pdf')
 print('Output')
 plt.close()
 
+# Print the content point cloud
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.view_init(elev=None, azim=None)
@@ -127,6 +140,7 @@ plt.title("Content Object")
 plt.savefig('content.pdf') 
 plt.close()
 
+# Print the style point cloud
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.view_init(elev=None, azim=None)
